1fa3301b7855d6c70aae8beb0b6d7b3d9702a44b
Eduardo <educonts08@gmail.com>
2022-10-26 22:33:46 -0300

    AppError created

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
+require('express-async-errors')
+
+const AppError = require('./utils/AppError')
+
 const express = require('express')
 
 const app = express()
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    messagem: 'Internal Server Error'
+  })
+})
 
 app.use(express.json())
 
