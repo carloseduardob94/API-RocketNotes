9b6fcbeae69a71ffb70cb191594fa532e85e2226
Eduardo <educonts08@gmail.com>
2022-10-27 21:56:11 -0300

    Error handling update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,16 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
 
-    response.json({ name, email, password })
+    if (!name) {
+      throw new AppError('Nome é obrigatório')
+    } else if (!email) {
+      throw new AppError('E-mail é obrigatório')
+    }
+
+    response.status(201).json({ name, email, password })
   }
 }
 
