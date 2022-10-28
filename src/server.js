369a7e2ef3bd460e8072916ce05875d9403bd3be
Eduardo <educonts08@gmail.com>
2022-10-28 14:21:52 -0300

    database connection and/or created

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const express = require('express')
 
 const AppError = require('./utils/AppError')
 const routes = require('./routes')
+const database = require('./database/sqlite')
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
