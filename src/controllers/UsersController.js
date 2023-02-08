4760becbc01f7fb4d176d48785b8127a45669eba
Eduardo <educonts08@gmail.com>
2023-02-08 18:39:03 -0300

    Creating method create users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,9 @@
+const { hash } = require('bcryptjs')
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
     if (!name) {
@@ -10,7 +12,25 @@ class UsersController {
       throw new AppError('E-mail é obrigatório')
     }
 
-    response.status(201).json({ name, email, password })
+    const database = await sqliteConnection()
+
+    const checkUser = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (checkUser) {
+      throw new AppError('Este e-mail já está cadastrado.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    )
+
+    return response.status(201).json()
   }
 }
 
