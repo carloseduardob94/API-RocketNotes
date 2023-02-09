9ad17d24fd122c6016beec723c689879698b80b4
Eduardo <educonts08@gmail.com>
2023-02-09 17:25:41 -0300

    Implement ORM Knex and config migrations

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
 
@@ -31,6 +31,59 @@ class UsersController {
     )
 
     return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado')
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('E-mail já está cadastrado.')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError(
+        'Você precisa informar a senha antiga para definir uma nova senha. '
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+      if (!checkOldPassword) {
+        throw new AppError('Senha inválida')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(
+      `
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?
+      `,
+      [user.name, user.email, user.password, id]
+    )
+
+    return response.json()
   }
 }
 
