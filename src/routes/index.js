62e0f884e758d57306e417b376653c1b7098de8f
Eduardo <educonts08@gmail.com>
2023-02-10 10:40:19 -0300

    Create NoteController, table note, table links, table tags in database

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 
-const userRoutes = require('./user.routes')
+const userRouter = require('./user.routes')
+const notesRouter = require('./notes.routes')
 
 const routes = Router()
 
-routes.use('/users', userRoutes)
+routes.use('/users', userRouter)
+routes.use('/notes', notesRouter)
 
 module.exports = routes
