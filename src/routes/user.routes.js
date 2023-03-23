a2722eed9f376f700765d5968b653db43809d0b9
Eduardo <educonts08@gmail.com>
2023-03-22 23:08:45 -0300

    update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,24 @@
 const { Router } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload.js')
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const userRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 userRoutes.post('/', usersController.create)
-userRoutes.put('/:id', usersController.update)
+userRoutes.put('/', ensureAuthenticated, usersController.update)
+userRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update
+)
 
 module.exports = userRoutes
--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 require('express-async-errors')
-const express = require('express')
-
 const AppError = require('./utils/AppError')
+const uploadConfig = require('./configs/upload')
 const migrationsRun = require('./database/sqlite/migrations')
+
+const cors = require('cors')
+const express = require('express')
 const routes = require('./routes')
+
 migrationsRun()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 
 app.use((error, request, response, next) => {
